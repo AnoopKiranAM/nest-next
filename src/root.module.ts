454a309e04dbd72fa9f088c7a3ef368088e9eb0f
Anoop Manjunatha <a_manjunatha@MXBHOSX884.local>
2023-01-13 17:14:38 +0100

    book resource building

--- src/root.module.ts
+++ src/root.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BookController } from './app.controller';
-import { BookService } from './book.service';
+
 import { ChatModule } from './chat.module';
 import { OrdersModule } from './orders.module';
 import { UsersModule } from './users.module';
@@ -9,7 +9,7 @@ import { UsersModule } from './users.module';
 @Module({
   imports: [UsersModule,OrdersModule,ChatModule],
   controllers: [BookController],
-  providers: [BookService],
+  providers: [],
   exports:[]
 })
 export class RootModule {
