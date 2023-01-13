84a0a0002120185bf6e10d7cdec23627928a41cc
Anoop Manjunatha <a_manjunatha@MXBHOSX884.local>
2023-01-13 16:09:53 +0100

    all modules created

---
+++ src/root.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ChatModule } from './chat.module';
+import { OrdersModule } from './orders.module';
+import { UsersModule } from './users.module';
+
+
+@Module({
+  imports: [UsersModule,OrdersModule,ChatModule],
+  controllers: [],
+  providers: [],
+  exports:[]
+})
+export class RootModule {
+  constructor(){
+console.log("root module")
+  }
+}
